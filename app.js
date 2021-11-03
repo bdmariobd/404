8fad57cee27e5e7c9ac32460b07373c57b2f6414
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-11-03 20:47:18 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+
+/* var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users'); */
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* app.use('/', indexRouter);
+app.use('/users', usersRouter); */
+
+app.listen(3000, function(err) {
+    if (err) {
+        console.error("No se pudo inicializar el servidor: " +
+            err.message);
+    } else {
+        console.log("Servidor arrancado en el puerto 3000");
+    }
+});
+
+app.get("/", (request, response) => {
+    response.status(200);
+    response.render("index");
+});
