59036116f446e8009b4df094e3a52cf77cd3c26b
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-11-14 22:48:59 +0100

    medio login

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+const session = require("express-session");
+const mysql = require("mysql");
+
+const mysqlSession = require("express-mysql-session");
+
+const DAOUsers = require("./persistence/DAOUsers.js")
 
 /* var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users'); */
@@ -17,14 +24,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* app.use('/', indexRouter);
 app.use('/users', usersRouter); */
 
-app.listen(3000, function(err) {
-    if (err) {
-        console.error("No se pudo inicializar el servidor: " +
-            err.message);
-    } else {
-        console.log("Servidor arrancado en el puerto 3000");
-    }
+const MySQLStore = mysqlSession(session);
+const sessionStorage = new MySQLStore(config.mysqlConfig);
+
+const middlewareSession = session({
+    saveUninitialized: false,
+    secret: "thisShouldBeEnvVariableLol05122000",
+    resave: false,
+    store: sessionStorage
 });
+
+app.use(middlewareSession);
+
+
+// Crear el pool de conexiones 
+const pool = mysql.createPool({
+    host: config.mysqlConfig.host,
+    user: config.mysqlConfig.user,
+    password: config.mysqlConfig.password,
+    database: config.mysqlConfig.database,
+});
+
+let daoUser = new DAOUsers(pool);
+//routes (TODO pasar a routers/controllers)
 
 app.get("/", (request, response) => {
     response.status(200);
@@ -36,7 +58,38 @@ app.get("/login", (request, response) => {
     response.render("login");
 });
 
+
+app.post("/login", (request, response) => {
+    daoUser.isUserCorrect(request.body.email, request.body.password,
+        (err, rows) => {
+            if (err) {
+                response.status(500);
+            } else {
+                response.status(200)
+                if (rows === null) {
+                    console.log("Email/pass not valid");
+                    response.render("login", null);
+                } else {
+                    request.session.currentUser = request.body.email;
+                    console.log(request)
+                    response.render("login", null);
+                }
+            }
+        });
+
+});
 app.get("/signup", (request, response) => {
     response.status(200);
     response.render("signup");
+});
+
+
+
+app.listen(config.port, function(err) {
+    if (err) {
+        console.error("No se pudo inicializar el servidor: " +
+            err.message);
+    } else {
+        console.log("Servidor arrancado en el puerto " + config.port);
+    }
 });
---
+++ persistence/DAOUsers.js
@@ -0,0 +1,57 @@
+"use strict";
+
+class DAOUsers {
+    constructor(pool) {
+        this.pool = pool;
+    }
+
+    isUserCorrect(email, password, callback) {
+        this.pool.getConnection(function(err, connection) {
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            } else {
+                connection.query(
+                    "SELECT * FROM user WHERE email = ? AND pass = ?", [email, password],
+                    function(err, rows) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        } else {
+                            if (rows.length === 0) {
+                                callback(null, null); //no está el usuario con el password proporcionado
+                            } else {
+                                callback(null, rows);
+                            }
+                        }
+                    });
+            }
+        });
+    }
+    create(email, password, callback) {
+
+        }
+        /* getUserImageName(email, callback) {
+            this.pool.getConnection(function(err, connection) {
+                if (err) {
+                    callback(new Error("Error de conexión a la base de datos"));
+                } else {
+                    connection.query(
+                        "SELECT img FROM user WHERE email = ? ", [email],
+                        function(err, rows) {
+                            connection.release(); // devolver al pool la conexión
+                            if (err) {
+                                callback(new Error("Error de acceso a la base de datos"));
+                            } else {
+                                if (rows.length === 0) {
+                                    callback(new Error("No existe el usuario")); //no está el usuario con el email proporcionado
+                                } else {
+                                    callback(null, rows[0].img);
+                                }
+                            }
+                        });
+                }
+            });
+        } */
+}
+
+module.exports = DAOUsers;
