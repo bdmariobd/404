42ac9bfdfa0845bf5d8d2cae559a70c0bdf47b8c
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-11-19 11:40:13 +0100

    sin responder backend

--- persistence/DAOQuestions.js
+++ persistence/DAOQuestions.js
@@ -1,7 +1,7 @@
 "use strict"
 
 
-class DAOTasks {
+class DAOQuestions {
     constructor(pool) {
         this.pool = pool;
     }
@@ -36,6 +36,37 @@ class DAOTasks {
             }
         });
     }
+
+    getAllUnansweredQuestions(callback) {
+        this.pool.getConnection(function(err, connection) {
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            } else {
+                let query = "SELECT q.id, q.id_user, q.title, q.body, q.views, q.date, q.likes, q.dislikes, GROUP_CONCAT(DISTINCT t.name) as tags " +
+                    "FROM ((question q join question_tag qt on q.id=qt.id_question) join tag t on qt.id_tag=t.id)" +
+                    "WHERE q.active = 1 AND 0 = (select count(*) FROM answer a WHERE a.question_id = q.id)" +
+                    " GROUP BY q.id ORDER BY q.date DESC";
+                connection.query(
+                    query,
+                    function(err, rows) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        } else {
+                            if (rows.length === 0) {
+                                callback(null, null); //no hay preguntas
+                            } else {
+                                rows.forEach(element => {
+                                    element.tags = element.tags.split(',');
+                                });
+                                callback(null, rows);
+                            }
+                        }
+                    }
+                );
+            }
+        });
+    }
 }
 
-module.exports = DAOTasks;
+module.exports = DAOQuestions;
