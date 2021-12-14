aefae0e131eb17672be93947e9f997d4012b9266
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-12-14 21:13:08 +0100

    dependencias

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 //necesario para formularios
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
